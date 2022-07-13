2eacc9c6f181fd64dfe9ead86bfb0d9109bffff0
plvhx <gandung@slackerc0de.us>
2022-07-13 23:15:02 +0700

    feat(src/init.go): Adding src/init.go

---
+++ src/init.go
@@ -0,0 +1,33 @@
+package src
+
+import (
+	// import some shit here.. :)
+)
+
+var (
+	hookChangeDirErr = errors.New("Failed to change git-hooks directory into .polar/hooks")
+	hookCreateDirFailErr = errors.New("Failed to create recursive directory .polar/hooks")
+)
+
+func Init() err {
+	args := []string{"config", "core.hooksPath", ".polar/hooks"}
+	err  := syscall.Exec("git", args, os.Environ())
+
+	if err != nil {
+		return hookChangeDirErr
+	}
+
+	_, err = os.Stat(".polar")
+
+	if err == nil {
+		return nil
+	}
+
+	err = os.MkdirAll(".polar/hooks")
+
+	if err != nil {
+		return hookCreateDirFailErr
+	}
+
+	return nil
+}
