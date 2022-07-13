a8619c0221dee75abb09717627ac36d2704cf488
plvhx <gandung@slackerc0de.us>
2022-07-13 23:26:16 +0700

    feat(cmd): Adding cmd/

---
+++ cmd/add.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"errors"
+	"os"
+
+	"github.com/plvhx/polar-bear/src"
+	"github.com/spf13/cobra"
+)
+
+var (
+	hookInitErr = errors.New("Run 'polar init' first.")
+)
+
+var addFunc = func(cmd *cobra.Command, args []string) {
+	if _, err := os.Stat(".polar"); err != nil {
+		panic(hookInitErr)
+	}
+
+	if err := src.CreateHook(args[0], args[1]); err != nil {
+		panic(err)
+	}
+}
+
+var addCmd = &cobra.Command{
+	Use: "add",
+	Short: "Adds a new hook",
+	Long: "Adds a new hook to polar and installs it.",
+	Args: cobra.ExactArgs(2),
+	Example: `polar add pre-commit "
+echo 'this is will run at pre-commit state.'
+"`,
+	Run: addFunc,
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
