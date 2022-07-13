a07616a090806d9d96a85ea9cf9f588eb6aa1ecb
plvhx <gandung@slackerc0de.us>
2022-07-13 23:14:31 +0700

    refactor(main): Move git-hooks initialization logic into src/init.go

--- main.go
+++ main.go
@@ -1,32 +1,8 @@
 package main
 
 import (
-	"errors"
-	"os"
-	"log"
-	"syscall"
+	// import some shit here.. :)
 )
-
-func init() {
-	args := []string{"config", "core.hooksPath", ".polar/hooks"}
-	err  := syscall.Exec("git", args, os.Environ())
-
-	if err != nil {
-		log.Fatal("Failed to change git-hooks directory into .polar/hooks")
-	}
-
-	_, err := os.Stat(".polar")
-
-	if err == nil {
-		return
-	}
-
-	err = os.MkdirAll(".polar/hooks")
-
-	if err != nil {
-		log.Fatal("Failed to create recursive directory .polar/hooks")
-	}
-}
 
 func main() {
 	// TODO: do some fancy shit here.. :)
