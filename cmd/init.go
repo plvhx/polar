4337e19c41b7c15659d76f8979f9623885381e7d
plvhx <gandung@slackerc0de.us>
2022-07-13 23:46:28 +0700

    feat(cmd/init.go): Adding cmd/init.go

---
+++ cmd/init.go
@@ -0,0 +1,26 @@
+// Copyright 2022 Paulus Gandung Prakosa <gandung@lists.infradead.org>
+// All rights reserved.
+
+package cmd
+
+import (
+	"github.com/plvhx/polar-bear/src"
+	"github.com/spf13/cobra"
+)
+
+var initFunc = func(cmd *cobra.Command, args []string) {
+	if err := src.Init(); err != nil {
+		panic(err)
+	}
+}
+
+var initCmd = &cobra.Command{
+	Use: "init",
+	Short: "Initialize polar-bear",
+	Long: "polar-bear is a tool to help you manage your git hooks.",
+	Run: initFunc,
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
